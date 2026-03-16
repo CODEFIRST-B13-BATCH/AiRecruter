3da50d4e048f0de7c4ff4b46b435717a72346da1
gayathri <you@example.com>
2026-03-16 17:20:26 +0530

    chart is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "../shared/navbar/navbar.component";
-
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { SuperAdminDashboardComponent } from "./components/super-admin-dashboard/super-admin-dashboard.component";
+ 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SuperAdminDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+         path:'superAdmindashboard', loadComponent:() => import('../app/components/super-admin-dashboard/super-admin-dashboard.component').then(m => m.SuperAdminDashboardComponent)
+}
+    
+];
