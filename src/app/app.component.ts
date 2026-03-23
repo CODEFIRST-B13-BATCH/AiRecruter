29bea7eeb39ec0869b4de8700513360dbc8169ba
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2026-03-23 13:45:33 +0530

    Updated Crop Management UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CropsComponent } from '../components/crops/crops/crops.component';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CropsComponent,NavbarComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AiRecruter';
+  title = 'MyhydroGrow';
 }
