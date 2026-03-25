3d6711444d4d1d31dda8dddc635aac8266654704
gayathri <you@example.com>
2026-03-25 13:32:38 +0530

    new navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { WorkerNavbarComponent } from "./shared/worker-navbar/worker-navbar.component";
 // import { SuperAdminDashboardComponent } from "./components/super-admin-dashboard/super-admin-dashboard.component";
  
  @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, WorkerNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,21 +9,21 @@ export const routes: Routes = [
     //         ),
     // },
 
-    {
-         path: 'crops-health',loadComponent:() => import('../app/components/crop-health/crop-health.component').then( (m) => m.CropHealthComponent),
-     },
+    // {
+    //      path: 'crops-health',loadComponent:() => import('../app/components/crop-health/crop-health.component').then( (m) => m.CropHealthComponent),
+    //  },
 
     // {
     //     path: 'crops-management',
     // },
 
-     {
-        path: 'alerts', loadComponent : () => import('../app/components/alert-notification/alert-notification.component').then((m) => m.AlertNotificationComponent),
-    },
+    //  {
+    //     path: 'alerts', loadComponent : () => import('../app/components/alert-notification/alert-notification.component').then((m) => m.AlertNotificationComponent),
+    // },
 
     // {
-    //     path: '',
-    // },
+    //     path: 'dashboard',loadComponent : () => import('../app/components/main-dashboard/main-dashboard.component').then((m) => m.MainDashboardComponent)
+    //  },
     // {
     //     path: '',
     // },
